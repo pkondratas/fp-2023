077bdd67fce031a59371c131a70042d69131dc5d
pkondratas <paulius.kondratas@gmail.com>
2023-09-26 17:43:25 +0300

    Trecias taskas

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -30,7 +30,36 @@ parseSelectAllStatement _ = error "parseSelectAllStatement not implemented"
 -- 3) implement the function which validates tables: checks if
 -- columns match value types, if rows sizes match columns,..
 validateDataFrame :: DataFrame -> Either ErrorMessage ()
-validateDataFrame _ = error "validateDataFrame ot implemented"
+validateDataFrame (DataFrame columns rows) = validateRows rows
+  where
+    validateRows :: [Row] -> Either ErrorMessage ()
+    validateRows [] = Right()
+    validateRows (r:rs) = 
+      case validateValueCount columns r of
+        Left error -> Left error
+        Right _ -> validateRows rs
+
+    validateValueCount :: [Column] -> [Value] -> Either ErrorMessage ()
+    validateValueCount [] [] = Right()
+    validateValueCount [] (_:_) = Left "Too many values"
+    validateValueCount (_:_) [] = Left "Not enough values"
+    validateValueCount (c:cs) (v:vs) 
+      | validateValueType c v = validateValueCount cs vs
+      | otherwise = Left "Type of value is incorrect"
+
+    
+    validateValueType :: Column -> Value -> Bool
+    validateValueType _ NullValue = True
+    validateValueType (Column _ colType) v =
+      case getValueType v of
+        Left _ -> False
+        Right valueType -> valueType == colType
+        
+    getValueType :: Value -> Either () ColumnType
+    getValueType (IntegerValue _) = Right IntegerType
+    getValueType (StringValue _) = Right StringType
+    getValueType (BoolValue _) = Right BoolType
+    getValueType _ = Left ()
 
 -- 4) implement the function which renders a given data frame
 -- as ascii-art table (use your imagination, there is no "correct"
