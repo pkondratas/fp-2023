2dfcd4d7fb87f3aec34fa5c1b3499530468e39af
dovix58 <119929439+dovix58@users.noreply.github.com>
2023-10-26 19:19:43 +0300

    CheckAll conditions padaryta

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -12,10 +12,11 @@ module Lib2
 where
 
 import Data.List ( elemIndex )
-import DataFrame (DataFrame (DataFrame), Column (Column), ColumnType (StringType), Value (StringValue))
+import DataFrame (DataFrame (DataFrame), Column (Column), ColumnType (StringType), Value (StringValue, IntegerValue, NullValue), Row)
 import InMemoryTables ( TableName, database )
 import Data.Char (toLower)
 import Data.Maybe (fromMaybe)
+import Data.List (isPrefixOf)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame.DataFrame)]
@@ -114,3 +115,86 @@ executeStatement (ShowTable table_name) =
 
 executeStatement ShowTables =
   Right $ DataFrame [Column "Tables" StringType] (map (\(name, _) -> [StringValue name]) database)
+
+
+
+checkCondition :: String -> String -> Row -> Either ErrorMessage Bool
+checkCondition condition table row = executeCondition (words condition)
+  where
+    -- Returns the operation value based on the operator provided in the condition string
+    executeCondition :: [String] -> Either ErrorMessage Bool
+    executeCondition (operand1 :  operator :  operand2 : _) =
+      case operator of
+        "=" -> Right (getOperandValue operand1 == getOperandValue operand2)
+        "<>" -> Right (getOperandValue operand1 /= getOperandValue operand2)
+        "!=" -> Right (getOperandValue operand1 /= getOperandValue operand2)
+        "<" -> Right (getOperandValue operand1 < getOperandValue operand2)
+        ">" -> Right (getOperandValue operand1 > getOperandValue operand2)
+        "<=" -> Right (getOperandValue operand1 <= getOperandValue operand2)
+        ">=" -> Right (getOperandValue operand1 >= getOperandValue operand2)
+        _ -> Left "Incorrect operator"
+ 
+    -- Returns an operand value based on the operand string (either a regular integer or a column value)
+    getOperandValue :: String -> Integer
+    getOperandValue opName =
+      if isInteger opName then
+        case reads opName of
+          [(intValue, _)] -> intValue
+      else
+        case getValueFromTable opName row of
+          IntegerValue intValue -> intValue
+          _ -> error "Column does not exist in this row"
+ 
+    -- Check if operand is an integer      
+    isInteger :: String -> Bool
+    isInteger str = case reads str :: [(Integer, String)] of
+      [(_, "")] -> True
+      _ -> False
+   
+    -- Get the value from the DataFrame row by column name
+    getValueFromTable :: String -> Row -> Value
+    getValueFromTable columnName currentRow =
+      let
+        -- Find the DataFrame by the tableName in the database
+        maybeDataFrame = lookup table database
+ 
+        -- Find the column index by columnName
+        maybeColumnIndex = maybeDataFrame >>= \(DataFrame columns _) ->
+          elemIndex columnName (map (\(Column name _) -> name) columns)
+ 
+        -- Get value from the row
+        value = maybe NullValue (\index -> fromMaybe NullValue (currentRow `atMay` index)) maybeColumnIndex
+      in
+        value
+ 
+    -- Get an element at a specific index in a list
+    atMay :: [a] -> Int -> Maybe a
+    atMay [] _ = Nothing
+    atMay (x:_) 0 = Just x
+    atMay (_:xs) n = atMay xs (n - 1)
+
+
+checkAll :: String -> String -> Row -> Either ErrorMessage Bool
+checkAll conditions tableName row
+    | null conditions = Left "Error: Conditions string is empty"
+    | otherwise = checkAllConditions (splitByAnd conditions) tableName row
+
+  where
+checkAllConditions :: [String] -> String -> Row -> Either ErrorMessage Bool
+checkAllConditions [] _ _ = Right True -- Base case: all conditions have been checked and are true
+checkAllConditions (condition:rest) tableName row =
+    case checkCondition condition tableName row of
+        Left errMsg -> Left errMsg -- If any condition fails, return the error message
+        Right True -> checkAllConditions rest tableName row -- If condition is true, check the rest of the conditions
+        Right False -> Right False -- If condition is false, return false immediately
+
+splitByAnd :: String -> [String]
+splitByAnd input = splitByWord "AND" input
+    where
+        splitByWord :: String -> String -> [String]
+        splitByWord _ [] = [""]
+        splitByWord word input@(x:xs)
+            | word `isPrefixOf` input = "" : splitByWord word (drop (length word) input)
+            | otherwise = (x : head rest) : tail rest
+            where
+                rest = splitByWord word xs
